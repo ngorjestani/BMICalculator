924d3b62be3e9fc05b58f0ffa90f16a3bc896b28
Nick Gorjestani <nickg2794@hotmail.com>
2022-04-11 20:32:17 -0500

    BMI calculator

---
+++ BMICalculator/BMICalculator.Droid/obj/Debug/110/android/src/crc64e054c1ebe7db4850/MainActivity.java
@@ -0,0 +1,55 @@
+package crc64e054c1ebe7db4850;
+
+
+public class MainActivity
+	extends crc643f46942d9dd1fff9.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("BMICalculator.Droid.MainActivity, BMICalculator.Droid", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("BMICalculator.Droid.MainActivity, BMICalculator.Droid", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("BMICalculator.Droid.MainActivity, BMICalculator.Droid", "System.Int32, mscorlib", this, new java.lang.Object[] { p0 });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ BMICalculator/BMICalculator/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.TizenSpecific;
+
+namespace BMICalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Btn_Clicked(object sender, System.EventArgs e)
+        {
+            int weight = Int32.Parse(Inp_weight.Text);
+            int height = Int32.Parse(Inp_height.Text);
+            int bmi = (weight * 703) / (height * height);
+            Lbl_BMI.Text = $"Your BMI is {bmi}.";
+        }
+    }
+}
